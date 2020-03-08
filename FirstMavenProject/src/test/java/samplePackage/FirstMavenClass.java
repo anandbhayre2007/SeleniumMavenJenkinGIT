af281e9b9b02893d65c5df2ccf5f0175884941e4
Anand Bhayre <anandbhayre2007@gmail.com>
2020-03-08 12:58:14 +0530

    committing code from CMD

---
+++ FirstMavenProject/src/test/java/samplePackage/FirstMavenClass.java
@@ -0,0 +1,23 @@
+package samplePackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstMavenClass {
+	
+	@Test
+	public void test1()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\drivers\\chromedriver.exe");
+		ChromeDriver dr= new ChromeDriver();
+		dr.manage().window().maximize();
+		dr.get("http://sampleapp.tricentis.com/101/");
+		
+		dr.findElement(By.id("nav_automobile")).click();
+		
+		dr.findElement(By.id("opendateofmanufacturecalender")).click();
+		
+	}
+
+}
